337f6c1f4928acf3a2aec806dfaf34bd2a5a7757
Loonis Pony Foo <linus@repl.it>
2019-07-19 21:38:13 +0000

    add some fruits

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ console.log('apricot seeds, apricot stems, this is from github -- remote')
 'this is going to cause a merge conflict the size of new york!'
 
 change from replit
+ensure test test test
+
+
+some more chagnes
+1. apples
+2. fruits
+3. oranges

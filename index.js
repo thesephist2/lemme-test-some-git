f33cd5e5ed885da31cc7598682afd358e48b87b1
Loonis Pony Foo <linus@repl.it>
2019-07-19 19:09:16 +0000

    change from replit

--- index.js
+++ index.js
@@ -5,9 +5,4 @@ console.log('apricot seeds, apricot stems, this is from github -- remote')
 
 'this is going to cause a merge conflict the size of new york!'
 
-soiafjsdiofjadsoifjioads
-
-
-conflicting change
-
 change from replit

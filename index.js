8cd4514b3530995a92694b11201c815235a5ba5d
Loonis Pony Foo <linus@repl.it>
2019-07-22 21:47:06 +0000

    Remove all except top 2 lines

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
 change i one
 my name is will robbins
-
-console.log('apricot seeds, apricot stems, this is from github -- remote')
-
-'this is going to cause a merge conflict the size of new york!'
-
-change from replit
-ensure test test test

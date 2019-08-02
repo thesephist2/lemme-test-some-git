90d96e8f172c78e6f39a760c2662dac77a199ba1
Loonis Pony Foo <linus@repl.it>
2019-08-02 00:30:55 +0000

    HALP

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-change i one
-my name is will robbins
-
-Neuralink
-some conflict-inducing changes
-Neuromancer
-more fonclits! isdjfoasjfoiasdjo
-SpaceX
-
-something to push!
-
-bleh bleh bleh.
+lol

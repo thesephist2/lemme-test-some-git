e3be64684665eed0f89c306258ebed449ce99de9
Loonis Pony Foo <linus@repl.it>
2019-08-05 19:26:40 +0000

    Remove zesty-specifc things

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ console.log('ZESTY ZESTY PENGUINS');
 
 const my_str = 'the quick brown fox jupmed over the lazy dog.'
 console.log(my_str);
-
-'change to index'
-
-'this commit should only live on zesty.'

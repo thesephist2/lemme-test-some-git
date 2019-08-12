760beac9d7657fe4ab5ba85fcc40235ac4bdffa4
Loonis Pony Foo <linus@repl.it>
2019-08-12 08:32:56 +0000

    Remove unnecessary whitespace
    - only for console.log bits.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 console.log('ZESTY ZESTY PENGUINS');
-
 const my_str = 'the quick brown fox jupmed over the lazy dog.'
 console.log(my_str);
-
 const str = 'removing git conflict!'
-
 console.log(my_str, str);
 console.log(str, my_str); // reversed!
 
